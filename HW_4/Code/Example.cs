406b6059007233c1d18b53d4cdc99d774d08ac83
Ernest <ernesto170696@gmail.com>
2021-05-02 23:34:24 +0300

    Add code

---
+++ HW_4/Code/Example.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+namespace Asteroids.Builder
+{
+    
+    internal sealed class Example : MonoBehaviour
+    {      [SerializeField] private Sprite _sprite;
+      
+        private void Start()
+        {
+
+            var gameObjectBuilder = new GameObjectBuilder();
+
+            GameObject player = gameObjectBuilder.Visual.Name("Roman").Sprite(_sprite).Physics.Rigidbody2D(5,0)
+                .BoxCollider2D(true);
+            
+        }
+        
+    }
+}
